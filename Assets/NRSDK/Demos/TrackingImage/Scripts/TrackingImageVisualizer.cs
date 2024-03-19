1b0916f63408feb13080c644f23381b06cc873e0
Yonsoo Seo <99024754+pzxcvasd@users.noreply.github.com>
2024-03-19 13:12:29 +0900

    initial commit

---
+++ Assets/NRSDK/Demos/TrackingImage/Scripts/TrackingImageVisualizer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NRKernal;
+
+public class TrackingImageVisualizer : MonoBehaviour
+{
+    public NRTrackableImage image;
+    public GameObject wand;
+
+    void Update()
+    {
+        Debug.Log("HELLO");
+        if(image == null)
+        {
+            wand.SetActive(false);
+            return;
+        }
+        var center = image.GetCenterPose();
+        transform.position = center.position;
+        transform.rotation = center.rotation;
+        wand.SetActive(true);
+    }
+}
