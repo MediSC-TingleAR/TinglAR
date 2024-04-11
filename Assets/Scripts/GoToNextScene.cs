9310418ce1b404966fa61f4ed70363169da4644c
Yonsoo Seo <99024754+pzxcvasd@users.noreply.github.com>
2024-04-11 16:55:11 +0900

    Scene Loading 오류 수정

--- Assets/Scripts/GoToNextScene.cs
+++ Assets/Scripts/GoToNextScene.cs
@@ -10,16 +10,17 @@ public class GoToNextScene : MonoBehaviour
 {
     private int nextsceneIndex; //다음 씬 인덱스 저장
     private GameObject nextSceneBtn;
+    private int currentSceneIndex;
 
     void Start()
     {   
         nextSceneBtn = gameObject;
-        // 다음 씬 유동적으로 계산.
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        // 다음 씬 유동적으로 계산
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         nextsceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
     }
 
-    public void OnClickNextBtn() {
+    public void onClickNextBtn() {
         SceneManager.LoadScene(nextsceneIndex);
     }
 }
