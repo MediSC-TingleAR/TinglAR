1663155667455694983ae9d31b59ebef3abac1b3
Yonsoo Seo <99024754+pzxcvasd@users.noreply.github.com>
2024-04-12 16:08:43 +0900

    부스트 UI 팝업 시 팔 캐릭터 비활성화

--- Assets/Scripts/EndSubGame.cs
+++ Assets/Scripts/EndSubGame.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class EndSubGame : MonoBehaviour
@@ -7,10 +8,12 @@ public class EndSubGame : MonoBehaviour
     public GameObject Done;
     private GameObject Enemies;
     private GameObject Count;
+    private GameObject ARObj;
     void Start()
     {
         Count = GameObject.Find("Canvas/freezeCount");
         Enemies = GameObject.Find("Enemies");
+        ARObj = GameObject.Find("ARGameObject");
 
         Done.SetActive(false);
 
@@ -19,6 +22,8 @@ public class EndSubGame : MonoBehaviour
     public void FinSubGame()
     {
         Done.SetActive(true);
+
+        ARObj.SetActive(false);
         Count.SetActive(false);
         Enemies.SetActive(false);
     }
