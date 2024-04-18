6fb04e2ca0836abf8495ac26941ecabcd42ecf08
Yonsoo Seo <99024754+pzxcvasd@users.noreply.github.com>
2024-04-18 14:23:41 +0900

    코루틴 오류 수정, FreezeCntText Position 수정

--- Assets/Scripts/SpawnEnemies.cs
+++ Assets/Scripts/SpawnEnemies.cs
@@ -6,52 +6,102 @@ using UnityEngine;
 public class SpawnEnemies : MonoBehaviour
 {
     public GameObject[] SpawnObjects; // 생성될 오브젝트
-    
-    [SerializeField]
-    private int maxCount = 30; //씬에 생성되는 오브젝트 최대 개수
-    public int spawnCount; //현재 생성된 오브젝트의 개수
-    private CameraSmoothFollow cameraposition;
+
+    private int maxCount = 15; //씬에 생성되는 오브젝트 최대 개수
+    public int spawnCount = 0; //현재 생성된 오브젝트의 개수
+    private int setFirstSpawnCount = 10;
+    private Vector3 cameraPosi;
+    private Quaternion cameraRott;
+    private bool isSpawning = false;
+
+    private Transform cameraInfo;
+    public GameObject cam;
 
     void Start()
     {
-        StartCoroutine(StartSpawning());
-        cameraposition = GameObject.Find("Canvas").GetComponent<CameraSmoothFollow>();
+        cameraInfo = cam.transform;
+        FirstSpawn();
+        gameObject.SetActive(false);
     }
     void Update()
     {
-        if (gameObject.activeSelf)
+        if (gameObject.activeSelf && !isSpawning)
+        {
+            cameraPosi = cameraInfo.position;
+            cameraRott = cameraInfo.rotation;
             StartCoroutine(StartSpawning());
+        }
+            
+    }
+
+    void FirstSpawn()
+    {
+        while (spawnCount < setFirstSpawnCount)
+        {
+            Vector3 spawnPosition = GetRandomSpawnPosition();
+            Quaternion spawnRotation = GetRandomSpawnRotation();
+
+            int randomIndex = Random.Range(0, SpawnObjects.Length);
+
+            GameObject spawnedObject = Instantiate(SpawnObjects[randomIndex],transform);
+
+            spawnedObject.transform.localPosition = spawnPosition;
+            spawnedObject.transform.localRotation = spawnRotation;
+            spawnedObject.SetActive(true);
+
+            Camera mainCamera = Camera.main;
+
+            spawnCount += 1;
+            
+        }
     }
 
     IEnumerator StartSpawning()
     {
+        isSpawning = true;
+
         while (spawnCount < maxCount)
         {
             Vector3 spawnPosition = GetRandomSpawnPosition();
+            Quaternion spawnRotation = GetRandomSpawnRotation();
             
             int randomIndex = Random.Range(0, SpawnObjects.Length);
 
             GameObject spawnedObject = Instantiate(SpawnObjects[randomIndex],transform);
             spawnedObject.transform.localPosition = spawnPosition;
-            spawnedObject.transform.localRotation = Quaternion.Euler(0f, 180f, 0f);
+            spawnedObject.transform.localRotation = spawnRotation;
             spawnedObject.SetActive(true);
 
             Camera mainCamera = Camera.main;
 
             spawnCount += 1;
 
+            Debug.Log("Printing");
             yield return new WaitForSeconds(3);
         }
+
+        isSpawning = false;
     }
 
     Vector3 GetRandomSpawnPosition()
     {
-        float xPos = Random.Range(-1.3f, 1.3f); 
-        float yPos = Random.Range(-1f, 1f);
+        float xPos = Random.Range(-1f, 1f); 
+        float yPos = Random.Range(-0.5f, 0.5f);
 
-        xPos = cameraposition.cameraPosi.x + xPos;
-        yPos = cameraposition.cameraPosi.y + yPos;
+        float xPos_n = cameraPosi.x + xPos;
+        float yPos_n = cameraPosi.y + yPos;
+        return new Vector3(xPos_n, yPos_n, 0);
+    }
 
-        return new Vector3(xPos, yPos, 0);
+    Quaternion GetRandomSpawnRotation()
+    {
+        float xRot = 0f;
+        float yRot = 180f;
+
+        float xRot_n = cameraRott.x + xRot;
+        float yRot_n = cameraRott.y + yRot;
+        Quaternion randomRotation = Quaternion.Euler(xRot_n, yRot_n, 0);
+
+        return randomRotation;
     }
 }
--- Assets/Scripts/SubGameManager.cs
+++ Assets/Scripts/SubGameManager.cs
@@ -9,12 +9,21 @@ public class SubGameManager : MonoBehaviour
 {
     private int count = 0;
     private TMP_Text FreezeCntText;
-
-    private EndSubGame endSubGame;
+    private GameObject FindSticker;
+    private GameObject Enemies;
+    private GameObject Done;
+    private GameObject ARObj;
     void Start()
     {
         FreezeCntText = GameObject.Find("Canvas/freezeCount").GetComponent<TMP_Text>();
-        endSubGame = GameObject.Find("subGameManager").GetComponent<EndSubGame>();
+        Enemies = GameObject.Find("Enemies");
+        FindSticker = GameObject.Find("Canvas/findSticker");
+        Done = GameObject.Find("Canvas/Done");
+        ARObj = GameObject.Find("ARGameObject");
+
+
+        FreezeCntText.gameObject.SetActive(false);
+        Done.SetActive(false);
 
         NRInput.AddClickListener(ControllerHandEnum.Right, ControllerButton.APP, () =>
             {
@@ -26,9 +35,7 @@ public class SubGameManager : MonoBehaviour
     void Update()
     {
         if (count == 20)
-        {
-            endSubGame.FinSubGame();
-        }
+            FinSubGame();
     }
 
     public void FreezeScore()
@@ -36,6 +43,23 @@ public class SubGameManager : MonoBehaviour
         count += 1;
         Debug.Log($"{count}");
         FreezeCntText.text = $"죽인 횟수 : {count}";
+    }
+
+    public void StartSubGame() //스티커 인식되고 게임 시작 (ARGameObject.cs)
+    {
+        FreezeCntText.gameObject.SetActive(true);
+        FindSticker.SetActive(false);
+        Enemies.SetActive(true);
+
+    }
+
+    public void FinSubGame()
+    {
+        Done.SetActive(true);
+        ARObj.SetActive(false);
+        FreezeCntText.gameObject.SetActive(false);
+        Enemies.SetActive(false);
+
     }
 
     
